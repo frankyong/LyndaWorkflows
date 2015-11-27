a470dd04dfdee59cf5c1cbf6a51c0374dcabe6e3
Frank Yong <frank.yong@gmail.com>
2015-11-27 11:11:04 -0500

    Added JS Concatentation to Dev folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),  // node.js command to bring in gulp library to create different tasks with gulp
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    gconcat = require('gulp-concat');
 
 /*
 gulp.task('log', function() {
@@ -11,10 +12,23 @@ gulp.task('log', function() {
 */
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+]; //this order is the order in which the files will get processed
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)  //first specify the original location of what I want to process.  Could be an array of files or an individual one     
 	.pipe(coffee({bare:true}) //coffee variable above and use one of the coffeescript options in the language     
 		.on('error', gutil.log)) //so that crashes won't stop execution of other gulp tasks     
 	.pipe(gulp.dest('components/scripts')) //where to send the file to once the processing is done
+});
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+	.pipe(gconcat('script.js'))  // index.html:   <script src="js/script.js"></script>
+	.pipe(gulp.dest('builds/development/js'))
+
 });
