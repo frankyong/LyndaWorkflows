4e89dca0585704d20d3def320fc69a46589202f8
Frank Yong <frank.yong@gmail.com>
2015-11-27 10:30:36 -0500

    Installed Gulp and Gulp-util

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp'),  // node.js command to bring in gulp library to create different tasks with gulp
+    gutil = require('gulp-util');
+
+
+gulp.task('log', function() {
+	gutil.log('workflows are awesome!');
+
+
+}); //create a taks called log
+
+
