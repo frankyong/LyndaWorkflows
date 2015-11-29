d4a3c201b0bfddd7ae876c750f5ab38ad4b37acf
Frank Yong <frank.yong@gmail.com>
2015-11-29 01:00:52 -0500

    minified json file

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var gulp = require('gulp'),  // node.js command to bring in gulp library to crea
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
     minifyHTML = require('gulp-minify-html'),
+    jsonminify = require('gulp-jsonminify'),
     concat = require('gulp-concat');
 
 var env, sassStyle, coffeeSources, jsSources, htmlSources, jsonSources, sassSources, outputDir
@@ -105,7 +106,7 @@ gulp.task('watch', function(){
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
 	gulp.watch('builds/development/*.html', ['html']);
-	gulp.watch(jsonSources, ['json']);
+	gulp.watch('builds/development/js/*.json', ['json']);
 });
 
 gulp.task('connect', function() {
@@ -123,6 +124,8 @@ gulp.task ('html', function() {
 });
 
 gulp.task ('json', function() {
-	gulp.src(jsonSources)
+	gulp.src('builds/development/js/*.json')
+	.pipe(gulpif(env === 'production', jsonminify()))
+	.pipe(gulpif(env === 'production', gulp.dest(outputDir + 'js')))
 	.pipe(connect.reload())
 });
